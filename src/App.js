19740380dbd9f15cbc1514449aa389c64a92b3bc
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-09-23 12:07:28 +0700

    setup sidebar commponet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
+import { Sidebar } from "./components/layout/Sidebar";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
-      Hello{" "}
+      <Sidebar />
     </div>
   );
 };
